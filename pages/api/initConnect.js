87141f7ff3a18a46f5052cd199832378ea796c63
Monkey Clouds <marcellusarthurs@gmail.com>
2022-04-30 12:25:07 +0100

    tweaked init connect file

--- pages/api/initConnect.js
+++ pages/api/initConnect.js
@@ -7,7 +7,7 @@ const handler = async (req, res) => {
 		const coll = db.collections("subscribedUsers");
 		const list = await coll.find();
 		if (req.method === "POST") {
-			res.status(200).json(list);
+			res.status(200).json({list});
 		}
   } catch (err) {
     res.status(400).send();
