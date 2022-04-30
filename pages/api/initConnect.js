7d3a48ea939b232f16fbeb37ee6145aa0f26a9a3
Monkey Clouds <marcellusarthurs@gmail.com>
2022-04-30 06:09:44 +0100

    tweaked init connect file

--- pages/api/initConnect.js
+++ pages/api/initConnect.js
@@ -3,9 +3,9 @@ import clientPromise from "../../lib/mongodb"
 const handler = async (req, res) => {
   const response = await clientPromise;
   const db = response.db();
-  const coll = db.collections("subscribedUsers");
+  const coll = await db.collections("subscribedUsers").find();
   if (req.method === "POST") {
-    res.status(200).json({coll});
+    res.status(200).json(coll);
   }
 }
 
